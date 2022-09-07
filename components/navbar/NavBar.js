6fdeca755b4ac2d5df05e3c87d34d5e7206a3453
Matias <matiasneil@gmail.com>
2022-09-07 16:36:25 -0300

    (fix) change <a> for <Link />

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 function NavBar() {
   return (
     <nav className="navbar is-light" role="navigation" aria-label="main navigation">
       <div className="navbar-brand ml-0">
-        <a className="navbar-item" href="/">
-          <img src="/assets/logo/sc-logo.png" width="112" height="28"></img>
-        </a>
+        <Link href="/">
+          <a className="navbar-item"><img src="/assets/logo/sc-logo.png" width="112" height="28"></img></a>
+        </Link>
       </div>
     </nav>
   );
