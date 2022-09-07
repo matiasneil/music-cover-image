00db19b0c646f249e36899d35a6f21cecafad953
Matias <matiasneil@gmail.com>
2022-09-07 16:00:55 -0300

    create navbar component

---
+++ components/navbar/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <nav className="navbar is-light" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand ml-0">
+        <a className="navbar-item" href="/">
+          <img src="/assets/logo/sc-logo.png" width="112" height="28"></img>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
